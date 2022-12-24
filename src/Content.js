1eb8c6f907203d5d01a414be4d6821c935339237
Daniyar <yeskaliyevdan@hotmail.com>
2022-12-23 22:24:58 -0800

    added buttons and event listeners

--- src/Content.js
+++ src/Content.js
@@ -4,13 +4,29 @@ const Content = () => {
         const int = Math.floor(Math.random() * 3)
         return names[int]
     }
-  return (
-    <main>
-        <p>
-            Hello {handleNameChange()}!
-        </p>
-    </main>
-  )
+
+    const handleClick = () => {
+      console.log('You clicked it')
+    }
+
+    const handleClick2 = (name) => {
+      console.log(`${name} was clicked`)
+    }
+
+    const handleClick3 = (e) => {
+      console.log(e.target.innerText)
+    }
+
+    return (
+      <main>
+          <p onDoubleClick={handleClick}>
+              Hello {handleNameChange()}!
+          </p>
+          <button onClick={handleClick}>Click it</button>
+          <button onClick={() => handleClick2('Danik')}>Click it</button>
+          <button onClick={(e) => handleClick3(e)}>Click it</button>
+      </main>
+    )
 }
 
 export default Content
