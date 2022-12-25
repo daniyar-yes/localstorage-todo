b2c8447265cbd282a42575300d25466679fbd441
Daniyar <yeskaliyevdan@hotmail.com>
2022-12-24 16:43:50 -0800

    re-arranged logic from content to itemlist and item components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
 import Header from './Header'
 import Content from './Content'
 import Footer from './Footer'
+import { useState } from 'react'
 
 function App() {
-  
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: true,
+      item: 'One half kilo bag of Cocoa covered almonds'
+    },
+    {
+      id: 2,
+      checked: false,
+      item: 'Deodorant'
+    },
+    {
+      id: 3,
+      checked: false,
+      item: 'Cuason'
+    },
+  ]);
+
+  const handleCheck = (id) => {
+    const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
+    setItems(listItems);
+    localStorage.setItem('shoppinglist', JSON.stringify(listItems))
+  }
+
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id)
+    setItems(listItems)
+    localStorage.setItem('shoppinglist', JSON.stringify(listItems))
+  }
+
 
   return (
     <div className="App">
-      <Header />
-      <Content />
-      <Footer />
+      <Header title="Grocery list"/>
+      <Content 
+        items={items}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}/>
+      <Footer length={items.length} />
       
     </div>
   );
--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,15 @@
-const Header = () => {
+const Header = ({title}) => {
     
 
   return (
     <header>
-        <h1>Groceries List</h1>
+        <h1>{title}</h1>
     </header>
   )
+}
+
+Header.defaultProps = {
+    title: 'default title'
 }
 
 export default Header
