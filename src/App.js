5c0348da4fb64e09679983f3c3b684d7be033214
Daniyar <yeskaliyevdan@hotmail.com>
2022-12-28 23:26:38 -0800

    Revert "isolated apiRequest in separate module"
    This reverts commit d42062aaebab9f80e0b179d21152a1ad4cb1ad66.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import AddItem from './AddItem'
 import Content from './Content'
 import Footer from './Footer'
 import { useState, useEffect } from 'react'
-import apiRequest from './apiRequest'
 
 function App() {
   const API_URL = 'http://localhost:3500/items';
@@ -36,50 +35,21 @@ function App() {
     
   }, [])
 
-  const addItem = async (item) => {
+  const addItem = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem];
     setItems(listItems);
-
-    const postOptions = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(myNewItem)
-    }
-    const result = await apiRequest(API_URL, postOptions);
-    if (result) setFetchError(result);
-
   }
 
-  const handleCheck = async (id) => {
+  const handleCheck = (id) => {
     const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
     setItems(listItems);
-
-    const myItem = listItems.filter((item) => item.id === id)
-    const updateOptions = {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ checked: myItem[0].checked})
-    };
-    const reqUrl = `${API_URL}/${id}`;
-    const result = await apiRequest(reqUrl, updateOptions);
-    if (result) setFetchError(result);
   }
 
-  const handleDelete = async (id) => {
+  const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id)
     setItems(listItems);
-
-    const deleteOptions = { method: 'DELETE' };
-    const reqUrl = `${API_URL}/${id}`;
-    const result = await apiRequest(reqUrl, deleteOptions);
-    if (result) setFetchError(result);
-
   }
 
   const handleSubmit = (e) => {
