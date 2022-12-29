c49e6d2dc657beb01e13456150567daccc7bd675
Daniyar <yeskaliyevdan@hotmail.com>
2022-12-28 23:26:46 -0800

    Revert "handled fetchError and isLoading via useState"
    This reverts commit 9f1a9b80c383cc8702e5bdc6bf786916dbab0451.

--- src/App.js
+++ src/App.js
@@ -6,34 +6,13 @@ import Footer from './Footer'
 import { useState, useEffect } from 'react'
 
 function App() {
-  const API_URL = 'http://localhost:3500/items';
-
-  const [items, setItems] = useState([]);
-  const [newItem, setNewItem] = useState('');
-  const [search, setSearch] = useState('');
-  const [fetchError, setFetchError] = useState(null);
-  const [isLoading, setIsLoading] = useState(true)
+  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')) || []);
+  const [newItem, setNewItem] = useState('')
+  const [search, setSearch] = useState('')
 
   useEffect(() => {
-    
-    const fetchItems = async () => {
-      try {
-        const response = await fetch(API_URL);
-        if (!response.ok) throw Error('Did not receive expected data');
-        const listItems = await response.json();
-        setItems(listItems);
-        setFetchError(null);
-      } catch (err) {
-        setFetchError(err.message);
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    setTimeout(() => {
-      fetchItems()
-    }, 2000)
-    
-  }, [])
+    localStorage.setItem('shoppinglist', JSON.stringify(items))
+  }, [items])
 
   const addItem = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
@@ -69,17 +48,11 @@ function App() {
       <SearchItem
         search={search}
         setSearch={setSearch}
-      />
-      <main>
-        {isLoading && <p>Loading Items...</p>}
-        {fetchError && <p style={{ color: 'red'}}>{`Error: ${fetchError}`}</p>}
-        {!fetchError && !isLoading &&
-          <Content 
-              items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
-              handleCheck={handleCheck}
-              handleDelete={handleDelete}/>
-        }
-      </main> 
+      />  
+      <Content 
+        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}/>
       <Footer length={items.length} />
       
     </div>
